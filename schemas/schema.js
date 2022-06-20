3b357026f568a1182894d7ec6d7588297443069c
Kevinmuriuki <kevinmuriukiwaweru@gmail.com>
2022-06-20 17:52:35 +0300

    Add experience schema

--- schemas/schema.js
+++ schemas/schema.js
@@ -6,6 +6,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import testimonials from './testimonials'
 import about from './about'
 import contact from './contact'
+import experience from './experience'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -16,6 +17,7 @@ export default createSchema({
   types: schemaTypes.concat([
     testimonials,
     about,
-    contact
+    contact,
+    experience,
   ]),
 })
