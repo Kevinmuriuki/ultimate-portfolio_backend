d53204688aa39609a61f695aa4f360914a19cb19
Kevinmuriuki <kevinmuriukiwaweru@gmail.com>
2022-06-20 17:32:02 +0300

    Add testimonial schema

---
+++ schemas/testimonials.js
@@ -0,0 +1,34 @@
+export default {
+    name: 'testimanials',
+    title: 'Testimonials',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+
+        },
+        {
+            name: 'company',
+            title: 'Company',
+            type: 'string',
+
+        },
+        {
+            name: 'imageurl',
+            title: 'ImgURL',
+            type: 'image',
+            options: {
+                hotspot: true
+            }
+
+        },
+        {
+            name: 'feedback',
+            title: 'Feedback',
+            type: 'string',
+
+        },
+    ]
+}
